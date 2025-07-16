664aa324240da152a9123af1720f25c1e2ab6ca2
BrandtOgden <ogden.brandt@gmail.com>
2025-07-16 00:50:50 -0400

    Initial Hello World OS

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+static MSG: &[u8] = b"I love you Grace";
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    let vga_buf = 0xb8000 as *mut u8;
+
+    for (i, &byte) in MSG.iter().enumerate() {
+        unsafe {
+            *vga_buf.offset(i as isize * 2) = byte;
+            *vga_buf.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
